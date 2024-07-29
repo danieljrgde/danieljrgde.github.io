2256400a7d0300c26e341a83ebcc0d96d771e388
daniel-deutsch <danieljorged1@gmail.com>
2024-07-29 22:39:27 +0200

    responsitivity

--- src/components/hoc/achievement/AchievementListItem.jsx
+++ src/components/hoc/achievement/AchievementListItem.jsx
@@ -5,17 +5,12 @@ import dayjs from 'dayjs';
 import { useState } from 'react';
 
 const styles = {
-    cardHeader: {
-        flexGrow: 1,
-        padding: 0,
-    },
     container: {
         display: "flex",
     },
-    subheader: (subheader) => ({
+    headerBox: {
         flexGrow: 1,
-        visibility: subheader ? "visible" : "hidden"
-    }),
+    },
 };
 
 const AchievementListItem = ({ achievement }) => {
@@ -28,18 +23,12 @@ const AchievementListItem = ({ achievement }) => {
 
     return (
         <>
-            <ListItemButton onClick={handleModal}>
-                <CardHeader
-                    sx={styles.cardHeader}
-                    title={
-                        <>
-                            <Typography variant="body2" display="inline">{achievement.title}</Typography>
-                            {achievement.subheader && <Typography variant="body2" color="text.secondary" display="inline">,&nbsp;{achievement.subheader}</Typography>}
-                        </>
-                    }
-                    action={<Typography variant="body2" color="text.secondary">{dayjs(achievement.date).format("MMM YYYY")}</Typography>}
-                />
-                
+            <ListItemButton onClick={handleModal} sx={styles.container}>
+                <Box sx={styles.headerBox}>
+                    <Typography variant="body2" display="inline">{achievement.title}</Typography>
+                    {achievement.subheader && <Typography variant="body2" color="text.secondary" display="inline">,&nbsp;{achievement.subheader}</Typography>}
+                </Box>
+                <Typography variant="body2" color="text.secondary">{dayjs(achievement.date).format("MMM YYYY")}</Typography>
             </ListItemButton>
             <AchievementModal open={isModalOpen} onClose={handleModal} achievement={achievement} />
         </>
