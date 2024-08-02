7bdf470c63126ca00c3d5a7640e09f206125b63f
danieljrgde <danieljrgde@gmail.com>
2024-08-02 20:22:22 +0200

    update workflow

--- src/components/pages/About.page.jsx
+++ src/components/pages/About.page.jsx
@@ -13,6 +13,7 @@ const AboutPage = () => {
 
     return (
         <Box>
+            TESTE
             <Typography variant="h6" color="text.primary" paragraph>{t("components.AboutPage.greetings")}</Typography>
             {paragraphs.map((paragraph, idx) => <Typography key={idx} variant="body1" color="text.secondary" paragraph>{paragraph}</Typography>)}
         </Box>
